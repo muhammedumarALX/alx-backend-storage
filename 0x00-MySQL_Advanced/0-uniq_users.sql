2c0f8adea02741463dd840def0ec7c05efb3c179
muhammed umar <muhammedumaralx@gmail.com>
2023-10-14 21:45:23 +0100

    Task 1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
--- creates a table `users`
-IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
-	CREATE TABLE users (
-		id SERIAL PRIMARY KEY,
-		email VARCHAR(255) Not NULL UNIQUE,
-		name VARCHAR(255)
-	);
-END IF;
+-- Craetes a table users
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
